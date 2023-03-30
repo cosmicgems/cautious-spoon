9958602ea929202d8630c20bf2099191e7de1447
Maliek Davis <maliekjdavis24@gmail.com>
2023-03-30 19:02:46 -0400

    overscroll index

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export default function App(props) {
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
     <ThemeProvider theme={theme} >
-    <div className='canvas' style={{height: '100vh'}}>
+    <div className='canvas' style={{height: '100vh', overscrollBehavior: 'contain'}}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
             
